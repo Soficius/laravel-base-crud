adb3fa96e8929137f6d5247a17c2c27d11921533
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-22 16:33:52 +0200

    fixed file for migration

--- database/migrations/2022_09_22_132818_create_comics_table.php
+++ database/migrations/2022_09_22_132818_create_comics_table.php
@@ -15,13 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 100);
+            $table->string('title', 1024);
             $table->text('description')->nullable();
             $table->string('thumb')->nullable();
-            $table->string('price', 10);
-            $table->string('series', 20);
+            $table->string('price', 1024);
+            $table->string('series', 1024);
             $table->date('sale_date');
-            $table->string('type', 20);
+            $table->string('type', 1024);
             $table->timestamps();
         });
     }
